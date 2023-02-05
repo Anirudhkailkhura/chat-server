2ea4995cb62658efd0fb11b0a981ac361cef843e
anirudh <anirudhkailkhura@gmail.com>
2023-02-05 12:33:46 +0530

    commit

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io  = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "http://chat-app-frontend-7rrv.onrender.com",
     Credential:true,
   }
 })
