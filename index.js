580f4e7cd14fbe49dc39e46c9e4cd44c1dd8693f
anirudh <anirudhkailkhura@gmail.com>
2023-02-05 11:30:32 +0530

    second commit

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/auth");
-
+const messageRoutes = require("./routes/messages");
 const app = express();
+const socket = require("socket.io");
+
 require("dotenv").config();
 
 mongoose.set('strictQuery', true);
@@ -23,9 +25,32 @@ mongoose
     console.log(err.message);
   });
 
-  app.use("/api/auth", authRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 
 const server = app.listen(process.env.PORT, () =>
   console.log(`Server started on ${process.env.PORT}`)
 );
+const io  = socket(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    Credential:true,
+  }
+})
+//nodejs global object
+global.onlineUsers = new Map(); // storing global users on map
+
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+    }
+  });
+});
--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,17 @@
 const {
     login,
-    register,
-    
+  register,
+  getAllUsers,
+  setAvatar,
     
 } = require("../controllers/userController");
 
 const router = require("express").Router();
 
 
-
+router.post("/login", login);
 router.post("/register", register);
-
+router.get("/allusers/:id", getAllUsers);
+router.post("/setavatar/:id", setAvatar);
 
 module.exports = router;
